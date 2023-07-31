12869071ecc7ef149f77bd55c4e12ad65d76534b
Devon Matsalla <dpmatsalla@hotmail.com>
2023-07-31 22:53:44 +1000

    Hello.js

---
+++ hello.js
@@ -0,0 +1,4 @@
+// JavaScript code to print a short message
+var message = "Hello, world!";
+console.log(message); // This will print the message to the browser's console
+document.write("<p>" + message + "</p>"); // This will print the message on the webpage
